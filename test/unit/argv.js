998731ea2417b292f60710180583560c5bed488b
cyberfunk <hello@cyberfunk.org>
2014-01-24 17:18:36 +0800

    Assert on default argument parsing.

---
+++ test/unit/argv.js
@@ -0,0 +1,108 @@
+var exec = require('child_process').exec;
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+var expect = require('chai').expect;
+
+var colors = {
+  bin: path.normalize(path.join(__dirname, '..', '..', 'bin', 'colors')),
+  log: path.normalize(path.join(__dirname, '..', '..', 'log', 'argv.log'))
+}
+
+describe('ttycolor:', function() {
+  function run(cmd, expected, done) {
+    var ps = exec(cmd,
+      function (error, stdout, stderr) {
+        var out = stdout.toString(), err = stderr.toString();
+        expect(error).to.be.null;
+        expect(out).to.be.a('string').that.equals('');
+        expect(err).to.be.a('string').that.equals('');
+        var contents = fs.readFileSync(colors.log).toString();
+        var lines = contents.split('\n');
+        lines.pop();
+        //console.dir(lines);
+        lines.forEach(function(line) {
+          var re = /\u001b/g;
+          var escaped = re.test(line);
+          //console.dir(line);
+          //console.dir(escaped);
+          expect(line).to.be.a('string');
+          expect(escaped).to.be.a('boolean').that.equals(expected);
+        });
+        done();
+      }
+    );
+  }
+  it('should write to file with no escape sequences (default: auto)',
+    function(done) {
+      var args = [];
+      var cmd = util.format('%s > %s 2>&1',
+        colors.bin + ' ' + args.join(' '), colors.log);
+      run(cmd, false, done);
+    }
+  );
+  it('should write to file with no escape sequences (--no-color)',
+    function(done) {
+      var args = ['--no-color'];
+      var cmd = util.format('%s > %s 2>&1',
+        colors.bin + ' ' + args.join(' '), colors.log);
+      run(cmd, false, done);
+    }
+  );
+  it('should write to file with no escape sequences (--color=never)',
+    function(done) {
+      var args = ['--color=never'];
+      var cmd = util.format('%s > %s 2>&1',
+        colors.bin + ' ' + args.join(' '), colors.log);
+      run(cmd, false, done);
+    }
+  );
+  it('should write to file with no escape sequences (--color never)',
+    function(done) {
+      var args = ['--color', 'never'];
+      var cmd = util.format('%s > %s 2>&1',
+        colors.bin + ' ' + args.join(' '), colors.log);
+      run(cmd, false, done);
+    }
+  );
+  it('should write to file with no escape sequences (--color=auto)',
+    function(done) {
+      var args = ['--color=auto'];
+      var cmd = util.format('%s > %s 2>&1',
+        colors.bin + ' ' + args.join(' '), colors.log);
+      run(cmd, false, done);
+    }
+  );
+  it('should write to file with no escape sequences (--color auto)',
+    function(done) {
+      var args = ['--color', 'auto'];
+      var cmd = util.format('%s > %s 2>&1',
+        colors.bin + ' ' + args.join(' '), colors.log);
+      run(cmd, false, done);
+    }
+  );
+  it('should write to file with escape sequences (--color)',
+    function(done) {
+      var args = ['--color'];
+      var cmd = util.format('%s > %s 2>&1',
+        colors.bin + ' ' + args.join(' '), colors.log);
+      run(cmd, true, done);
+    }
+  );
+  it('should write to file with escape sequences (--color=always)',
+    function(done) {
+      var args = ['--color=always'];
+      var cmd = util.format('%s > %s 2>&1',
+        colors.bin + ' ' + args.join(' '), colors.log);
+      run(cmd, true, done);
+    }
+  );
+  it('should write to file with escape sequences (--color always)',
+    function(done) {
+      var args = ['--color', 'always'];
+      var cmd = util.format('%s > %s 2>&1',
+        colors.bin + ' ' + args.join(' '), colors.log);
+      run(cmd, true, done);
+    }
+  );
+})
