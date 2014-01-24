32e326cf3b19a1ef21baf99b24ba200f920ab89d
cyberfunk <hello@cyberfunk.org>
2014-01-24 19:16:08 +0800

    Tidy argv test cases.

--- test/unit/argv.js
+++ test/unit/argv.js
@@ -4,37 +4,16 @@ var path = require('path');
 var util = require('util');
 var expect = require('chai').expect;
 
-var colors = {
-  bin: path.normalize(path.join(__dirname, '..', '..', 'bin', 'colors')),
-  log: path.normalize(path.join(__dirname, '..', '..', 'log', 'argv.log'))
-}
+var run = require('../util/run');
+var exe = path.normalize(path.join(__dirname, '..', '..', 'bin', 'colors'));
+var log = path.normalize(path.join(__dirname, '..', '..', 'log', 'argv.log'))
 
 describe('argv:', function() {
-  function run(cmd, expected, done) {
-    var ps = exec(cmd,
-      function (error, stdout, stderr) {
-        var out = stdout.toString(), err = stderr.toString();
-        expect(error).to.be.null;
-        expect(out).to.be.a('string').that.equals('');
-        expect(err).to.be.a('string').that.equals('');
-        var contents = fs.readFileSync(colors.log).toString();
-        var lines = contents.split('\n');
-        lines.pop();
-        lines.forEach(function(line) {
-          var re = /\u001b/g;
-          var escaped = re.test(line);
-          expect(line).to.be.a('string');
-          expect(escaped).to.be.a('boolean').that.equals(expected);
-        });
-        done();
-      }
-    );
-  }
   it('should write to file with no escape sequences (default: auto)',
     function(done) {
       var args = [];
       var cmd = util.format('%s > %s 2>&1',
-        colors.bin + ' ' + args.join(' '), colors.log);
+        exe + ' ' + args.join(' '), log);
       run(cmd, false, done);
     }
   );
@@ -42,7 +21,7 @@ describe('argv:', function() {
     function(done) {
       var args = ['--no-color'];
       var cmd = util.format('%s > %s 2>&1',
-        colors.bin + ' ' + args.join(' '), colors.log);
+        exe + ' ' + args.join(' '), log);
       run(cmd, false, done);
     }
   );
@@ -50,7 +29,7 @@ describe('argv:', function() {
     function(done) {
       var args = ['--color=never'];
       var cmd = util.format('%s > %s 2>&1',
-        colors.bin + ' ' + args.join(' '), colors.log);
+        exe + ' ' + args.join(' '), log);
       run(cmd, false, done);
     }
   );
@@ -58,7 +37,7 @@ describe('argv:', function() {
     function(done) {
       var args = ['--color', 'never'];
       var cmd = util.format('%s > %s 2>&1',
-        colors.bin + ' ' + args.join(' '), colors.log);
+        exe + ' ' + args.join(' '), log);
       run(cmd, false, done);
     }
   );
@@ -66,7 +45,7 @@ describe('argv:', function() {
     function(done) {
       var args = ['--color=auto'];
       var cmd = util.format('%s > %s 2>&1',
-        colors.bin + ' ' + args.join(' '), colors.log);
+        exe + ' ' + args.join(' '), log);
       run(cmd, false, done);
     }
   );
@@ -74,7 +53,7 @@ describe('argv:', function() {
     function(done) {
       var args = ['--color', 'auto'];
       var cmd = util.format('%s > %s 2>&1',
-        colors.bin + ' ' + args.join(' '), colors.log);
+        exe + ' ' + args.join(' '), log);
       run(cmd, false, done);
     }
   );
@@ -82,7 +61,7 @@ describe('argv:', function() {
     function(done) {
       var args = ['--color'];
       var cmd = util.format('%s > %s 2>&1',
-        colors.bin + ' ' + args.join(' '), colors.log);
+        exe + ' ' + args.join(' '), log);
       run(cmd, true, done);
     }
   );
@@ -90,7 +69,7 @@ describe('argv:', function() {
     function(done) {
       var args = ['--color=always'];
       var cmd = util.format('%s > %s 2>&1',
-        colors.bin + ' ' + args.join(' '), colors.log);
+        exe + ' ' + args.join(' '), log);
       run(cmd, true, done);
     }
   );
@@ -98,7 +77,7 @@ describe('argv:', function() {
     function(done) {
       var args = ['--color', 'always'];
       var cmd = util.format('%s > %s 2>&1',
-        colors.bin + ' ' + args.join(' '), colors.log);
+        exe + ' ' + args.join(' '), log);
       run(cmd, true, done);
     }
   );
---
+++ test/util/run.js
@@ -0,0 +1,27 @@
+var exec = require('child_process').exec;
+var fs = require('fs');
+var path = require('path');
+var expect = require('chai').expect;
+
+var log = path.normalize(path.join(__dirname, '..', '..', 'log', 'argv.log'))
+
+module.exports = function run(cmd, expected, done) {
+  var ps = exec(cmd,
+    function (error, stdout, stderr) {
+      var out = stdout.toString(), err = stderr.toString();
+      expect(error).to.be.null;
+      expect(out).to.be.a('string').that.equals('');
+      expect(err).to.be.a('string').that.equals('');
+      var contents = fs.readFileSync(log).toString();
+      var lines = contents.split('\n');
+      lines.pop();
+      lines.forEach(function(line) {
+        var re = /\u001b/g;
+        var escaped = re.test(line);
+        expect(line).to.be.a('string');
+        expect(escaped).to.be.a('boolean').that.equals(expected);
+      });
+      done();
+    }
+  );
+}
