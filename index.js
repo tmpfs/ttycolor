fe401d5c9715ea03a6a9b088078deda0256dc26e
cyberfunk <hello@cyberfunk.org>
2014-01-25 10:53:00 +0800

    Move ansi codes to a module.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var util = require('util');
 
+var definition = require('./lib/ansi-codes');
 var parse = require('./lib/parse');
 var stream = require('./lib/stream');
 var styles = require('./lib/styles');
@@ -11,41 +12,6 @@ var cache = {}, stash = {
   info: console.info,
   error: console.error,
   warn: console.warn
-}
-
-var definition = {
-  colors: {
-    normal          : 39,
-    white           : 37,
-    black           : 30,
-    blue            : 34,
-    cyan            : 36,
-    green           : 32,
-    yellow          : 33,
-    magenta         : 35,
-    red             : 31
-  },
-  bg: {
-    colors: {
-      normal        : 49,
-      white         : 47,
-      black         : 40,
-      blue          : 44,
-      cyan          : 46,
-      green         : 42,
-      yellow        : 43,
-      magenta       : 45,
-      red           : 41
-    }
-  },
-  attrs: {
-    bright          : 1,
-    dim             : 2,
-    italic          : 3,
-    underline       : 4,
-    blink           : 5,
-    reverse         : 7
-  }
 }
 
 var ANSI_OPEN = '\u001b[';
