738d515c93b619c8603d6bb91e168843bdb7b248
cyberfunk <hello@cyberfunk.org>
2014-01-25 10:57:02 +0800

    Move color builder class to module.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 var util = require('util');
 
-var definition = require('./lib/ansi-codes');
+var AnsiColor = require('./lib/color');
+var definition = AnsiColor.codes, stringify = AnsiColor.stringify;
 var parse = require('./lib/parse');
 var stream = require('./lib/stream');
 var styles = require('./lib/styles');
@@ -12,45 +13,6 @@ var cache = {}, stash = {
   info: console.info,
   error: console.error,
   warn: console.warn
-}
-
-var ANSI_OPEN = '\u001b[';
-var ANSI_FINAL = 'm';
-var ANSI_CLOSE_CODE = '0';
-var ANSI_CLOSE = ANSI_OPEN + ANSI_CLOSE_CODE + ANSI_FINAL;
-
-/**
- *  Open an escape sequence.
- *
- *  @param code The color code.
- *  @param attr An optional attribute code.
- */
-function open(code, attr) {
-  return  attr ? ANSI_OPEN + attr + ';' + code + ANSI_FINAL
-    : ANSI_OPEN + code + ANSI_FINAL;
-}
-
-/**
- *  Concatenate a close sequence.
- *
- *  @param value The value to close.
- */
-function close(value, tag) {
-  return value + (tag ? tag : ANSI_CLOSE);
-}
-
-/**
- *  Low-level method for creating escaped string sequences.
- *
- *  @param value The value to escape.
- *  @param code The color code.
- *  @param attr An optional attribute code.
- *  @param tag A specific closing tag to use.
- */
-function stringify(value, code, attr, tag) {
-  var s = open(code, attr);
-  s +=  close(value, tag);
-  return s;
 }
 
 /**
@@ -107,107 +69,6 @@ function proxy(options, format) {
   replacements.unshift(format);
   return method.apply(options.scope ? options.scope : console, replacements);
 }
-
-/**
- *  Chainable color builder.
- *
- *  @param value The underlying value to be escaped.
- *  @param key The key for the code lookup.
- *  @param parent A parent color instance.
- */
-var AnsiColor = function(value, key, parent){
-  this.t = definition.colors;
-  this.v = value;
-  this.k = key;
-  this.p = parent;
-  this.a = null;
-}
-
-/**
- *  Retrieve list of parent instances.
- */
-AnsiColor.prototype.parents = function() {
-  var list = [this], p = this.p;
-  while(p) {
-    if(p) {
-      list.push(p);
-    }
-    p = p.p;
-  }
-  list.reverse();
-  return list;
-}
-
-/**
- *  Retrieve a start escape sequence.
- *
- *  @param tty Whether the output stream is a terminal.
- */
-AnsiColor.prototype.start = function(tty) {
-  if(!tty) return '';
-  var list = this.parents(), i, p;
-  var o = ANSI_CLOSE;
-  for(i = 0;i < list.length;i++){
-    p = list[i];
-    if(!p.k) continue;
-    o += open(p.t[p.k], p.a);
-  }
-  return o;
-}
-
-/**
- *  Retrieve an escape sequence from the chain.
- *
- *  @param tty Whether the output stream is a terminal.
- *  @param tag A specific closing tag to use.
- */
-AnsiColor.prototype.valueOf = function(tty, tag) {
-  if(!tty) return this.v;
-  var list = this.parents(), i, p;
-  for(i = 0;i < list.length;i++){
-    p = list[i];
-    if(!p.k) continue;
-    this.v = stringify(this.v, p.t[p.k], p.a, tag);
-  }
-  return this.v;
-}
-
-AnsiColor.prototype.__defineGetter__('bg', function() {
-  var ansi = new AnsiColor(this.v, this.k, this);
-  ansi.t = definition.bg.colors;
-  return ansi;
-});
-
-// attributes
-Object.keys(definition.attrs).forEach(function (k) {
-  AnsiColor.prototype.__defineGetter__(k, function () {
-    if(this.a) {
-      var ansi = new AnsiColor(this.v, this.k || 'normal', this);
-      ansi.a = definition.attrs[k];
-      return ansi;
-    }
-    this.a = definition.attrs[k];
-    this.k = this.k || 'normal';
-    return this;
-  });
-});
-
-// colors
-Object.keys(definition.colors).forEach(function (k) {
-  AnsiColor.prototype.__defineGetter__(k, function () {
-    // reset the background color chain after color method invocation
-    // allows invoking foreground colors after background colors
-    if(this.k && this.t == definition.bg.colors && this.p && !this.p.k) {
-      return new AnsiColor(this.v, k, this);
-    }
-    this.k = k;
-    // allow color functions after multiple attribute chains
-    if(this.p && this.p.a && this.p.k == 'normal') {
-      this.p.k = this.k;
-    }
-    return this;
-  });
-});
 
 function isatty(tty, mode) {
   if(mode == parse.always) return true;
