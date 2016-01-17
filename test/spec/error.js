5440f0f87357504d4daed3f42b96260936db48b6
muji <freeformsystems@gmail.com>
2016-01-17 19:26:52 +0800

    Add error stream test specs.

---
+++ test/spec/error.js
@@ -0,0 +1,27 @@
+var expect = require('chai').expect
+  , ttycolor = require('../..');
+
+describe('ttycolor:', function() {
+
+  afterEach(function(done) {
+    ttycolor.stderr(false);
+    done();
+  })
+
+  it('should use error stream', function(done) {
+    expect(ttycolor.stderr()).to.eql(false);
+    ttycolor.stderr(true);
+    expect(ttycolor.stderr()).to.eql(true);
+    ttycolor.revert();
+    done();
+  });
+
+  it('should use error stream calling defaults', function(done) {
+    expect(ttycolor.stderr()).to.eql(false);
+    ttycolor.defaults(ttycolor.styles, true);
+    expect(ttycolor.stderr()).to.eql(true);
+    ttycolor.revert();
+    done();
+  });
+
+})
