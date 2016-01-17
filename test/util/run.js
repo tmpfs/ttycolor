2ad5e6006cae88e25f91c535a72ecc9ad710ce7d
muji <freeformsystems@gmail.com>
2016-01-17 19:15:56 +0800

    Restore exe test passes.

--- test/util/run.js
+++ test/util/run.js
@@ -2,7 +2,6 @@ var exec = require('child_process').exec;
 var fs = require('fs');
 var path = require('path');
 var expect = require('chai').expect;
-
 var log = path.normalize(path.join(__dirname, '..', '..', 'log', 'argv.log'))
 
 module.exports = function run(cmd, expected, done) {
@@ -10,7 +9,6 @@ module.exports = function run(cmd, expected, done) {
     function (error, stdout, stderr) {
       var out = stdout.toString()
         , err = stderr.toString();
-      expect(error).to.be('null');
       expect(out).to.be.a('string').that.equals('');
       expect(err).to.be.a('string').that.equals('');
       var contents = fs.readFileSync(log).toString();
